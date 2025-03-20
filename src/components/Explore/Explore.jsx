c03d94d77032489f0c1288d574a94002f9f71608
Laura-Teodora Misicu <laurateodora23@gmail.com>
2025-03-20 06:53:20 +0200

    Browse by type functionality

--- src/components/Explore/Explore.jsx
+++ src/components/Explore/Explore.jsx
@@ -2,28 +2,53 @@ import React from "react"
 import "./Explore.css"
 import brandData from '../../data/premiumBrandsData'
 import typeData from '../../data/carTypeData'
+import carsData from "../../data/carsData"
+import Card from '../Featured/Card'
+
+import { useState } from "react"
 
 export default function Explore(props) {
+    const [selectedBrand, setSelectedBrand] = useState("");
+    const [selectedType, setSelectedType] = useState("");
+
+
     const brands = brandData.map(({id, img, brand}) =>
-        <li key={id}>
+        <li key={id} 
+            onClick={() => setSelectedBrand(brand)}
+            className={selectedBrand === brand ? "selected" : ""}
+        >
             <img src={img}  alt={`${brand} logo`}/>
             <h5>{brand}</h5>
         </li>)
     const types = typeData.map(({id, img, type}) =>
-        <li key={id}>
+        <li key={id}
+            onClick={() => setSelectedType(type)}
+            className={selectedType === type ? "selected" : ""}>
             <img src={img}  alt={`${type} logo`}/>
             <h5>{type}</h5>
         </li>)
+
+    const filteredCars = selectedType ? carsData.filter(car => car.type === selectedType) : []
+    const cars = filteredCars.map(car => <Card key={car.id} img={car.img} car={car.car} details={car.details} price={car.price} />)
+
     return (
         <div className="explore-section">
             <h1>Browse by Type</h1>
             <ul className="type-list">
                 {types}
             </ul>
+
+            <div className="card-list">
+                {cars}
+            </div>
+
             <h1>Explore Our Premium Brands</h1>
             <ul className="brand-list">
                 {brands}
             </ul>
+
+            
+
 
         </div>
         
---
+++ src/data/carsData.js
@@ -0,0 +1,66 @@
+export default [
+    {
+        id: 1,
+        img: "/assets/SUV/audi-q3_212_1.jpg",
+        type: "suv",
+        car: "Ford Transit - 2021",
+        details: "2500 Miles • Diesel • Manual",
+        price: "$22,000"
+    },
+    {
+        id: 2,
+        img: "/assets/SUV/audi-q7_214_1.jpg",
+        type: "suv",
+        car: "Ford Transit - 2021",
+        details: "2500 Miles • Diesel • Manual",
+        price: "$22,000"
+    },
+    {
+        id: 3,
+        img: "/assets/SUV/bmw-x1_307_1.jpg",
+        type: "suv",
+        car: "Ford Transit - 2021",
+        details: "2500 Miles • Diesel • Manual",
+        price: "$22,000"
+    },
+    {
+        id: 4,
+        img: "/assets/SUV/bmw-x3_309_1.jpg",
+        type: "suv",
+        car: "Ford Transit - 2021",
+        details: "2500 Miles • Diesel • Manual",
+        price: "$22,000"
+    },
+    {
+        id: 5,
+        img: "/assets/Sedan/alfa-romeo-156_17_1.jpg",
+        type: "Sedan",
+        car: "Ford Transit - 2021",
+        details: "2500 Miles • Diesel • Manual",
+        price: "$22,000"
+    },
+    {
+        id: 6,
+        img: "/assets/Sedan/audi-s4_125_1.jpg",
+        type: "Sedan",
+        car: "Ford Transit - 2021",
+        details: "2500 Miles • Diesel • Manual",
+        price: "$22,000"
+    },
+    {
+        id: 7,
+        img: "/assets/Sedan/chevrolet-nubira_374_1.jpg",
+        type: "Sedan",
+        car: "Ford Transit - 2021",
+        details: "2500 Miles • Diesel • Manual",
+        price: "$22,000"
+    },
+    {
+        id: 8,
+        img: "/assets/Sedan/chrysler-sebring_394_1.jpg",
+        type: "Sedan",
+        car: "Ford Transit - 2021",
+        details: "2500 Miles • Diesel • Manual",
+        price: "$22,000"
+    }
+]
